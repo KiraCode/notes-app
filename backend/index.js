6aa0fa3cc10a169b0ec03cf92de3e0c4614c63f0
tejas git <d9489079@gmail.com>
2025-06-13 17:31:28 +0530

    added backend code for signup page

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,18 @@
 import express from "express";
+import cors from "cors";
+import connectToMongoDB from "./db/db.js";
+
+import authRouter from "./routes/auth.js";
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(5000, ()=>{
-    console.log('backend is running');
-    
-})
+connectToMongoDB();
+
+app.use("/api/auth", authRouter);
+
+app.listen(5000, () => {
+  console.log("backend is running");
+});
