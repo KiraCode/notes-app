de6ecf7a7315b08d42bf1529222e595f8e80506a
tejas git <d9489079@gmail.com>
2025-06-15 14:37:49 +0530

    delete functionliy is added for the note

--- backend/routes/note.js
+++ backend/routes/note.js
@@ -48,4 +48,15 @@ router.put("/:id", async (req, res) => {
   }
 });
 
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedNote = await Note.findByIdAndDelete(id);
+    return res.status(200).json({ success: true, updatedNote });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, message: "Can't Delete Notes" });
+  }
+});
 export default router;
