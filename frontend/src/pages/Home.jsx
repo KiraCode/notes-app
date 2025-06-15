a879b9666ccb342cf92f0bbc3aa703d342d47ee6
tejas git <d9489079@gmail.com>
2025-06-15 09:56:16 +0530

    frontend addNote button functionality is done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,16 +7,30 @@ const Home = () => {
   const closeModal = () => {
     setModealOpen(false);
   };
+
+  const addNote = async ({ title, description }) => {
+    try {
+      const response = await axios.post("http://localhost:5000/api/note/add", {
+        title,
+        description,
+      });
+      if (response.data.success) {
+        closeModal();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="bg-gray-100 min-h-screen">
       <Navbar />
       <button
         onClick={() => setModealOpen(true)}
-        className="fixed right-4 bottom-4 text-2xl bg-teal-500 text-white font-bold p-4 rounded-full "
+        className="fixed right-4 bottom-4 text-2xl bg-teal-500 text-white font-bold p-4 rounded-none "
       >
         +
       </button>
-      {isModalOpen && <NoteModal closeModal={closeModal} />}
+      {isModalOpen && <NoteModal closeModal={closeModal} addNote={addNote} />}
     </div>
   );
 };
