0c523fecc1b92644808e7b2e2748d3c192e69ece
tejas git <d9489079@gmail.com>
2025-06-15 13:10:44 +0530

    added notecard to display notes in grid fromat in homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,11 +2,35 @@ import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import NoteModal from "../components/NoteModal";
 import axios from "axios";
+import { useEffect } from "react";
+import NoteCard from "../components/NoteCard";
 
 const Home = () => {
-  const [isModalOpen, setModealOpen] = useState(false);
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [notes, setNotes] = useState([]);
+  const [currentNote, setCurrentNote] = useState();
+
   const closeModal = () => {
-    setModealOpen(false);
+    setModalOpen(false);
+  };
+
+  const fetchNotes = async () => {
+    try {
+      const { data } = await axios.get("http://localhost:5000/api/note");
+      console.log(data);
+      setNotes(data.notes);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  const onEdit = (note) => {
+    setCurrentNote(note);
+    setModalOpen(true);
   };
 
   const addNote = async (title, description) => {
@@ -28,6 +52,7 @@ const Home = () => {
 
       if (response.data.success) {
         closeModal();
+        fetchNotes();
       }
     } catch (error) {
       console.log(error);
@@ -36,13 +61,24 @@ const Home = () => {
   return (
     <div className="bg-gray-100 min-h-screen">
       <Navbar />
+      <div className="grid grid-cols-1 md:grid-cols-3 px-8 pt-4 gap-2">
+        {notes.map((note) => (
+          <NoteCard note={note} onEdit={onEdit} />
+        ))}
+      </div>
       <button
-        onClick={() => setModealOpen(true)}
+        onClick={() => setModalOpen(true)}
         className="fixed right-4 bottom-4 text-2xl bg-teal-500 text-white font-bold p-4 rounded-full "
       >
         +
       </button>
-      {isModalOpen && <NoteModal closeModal={closeModal} addNote={addNote} />}
+      {isModalOpen && (
+        <NoteModal
+          closeModal={closeModal}
+          addNote={addNote}
+          currentNote={currentNote}
+        />
+      )}
     </div>
   );
 };
