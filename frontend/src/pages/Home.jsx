b0af065b1a24925a8fba29cd0159dd3ea868b018
tejas git <d9489079@gmail.com>
2025-06-15 09:27:21 +0530

    added notemodal for 'note add' button in home screen

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar";
+import NoteModal from "../components/NoteModal";
 
 const Home = () => {
+  const [isModalOpen, setModealOpen] = useState(false);
+  const closeModal = () => {
+    setModealOpen(false);
+  };
   return (
-    <div>
+    <div className="bg-gray-100 min-h-screen">
       <Navbar />
+      <button
+        onClick={() => setModealOpen(true)}
+        className="fixed right-4 bottom-4 text-2xl bg-teal-500 text-white font-bold p-4 rounded-full "
+      >
+        +
+      </button>
+      {isModalOpen && <NoteModal closeModal={closeModal} />}
     </div>
   );
 };
