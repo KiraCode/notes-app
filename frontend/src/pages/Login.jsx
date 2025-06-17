5ae85e67959cbaff1b915a8b53026a89f591e523
tejas git <d9489079@gmail.com>
2025-06-17 11:59:16 +0530

    chnaged the import name in frontend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const response = await axios.post(
-        `${import.meta.BACKEND_URL}/api/auth/login`,
+        `${import.meta.env.BACKEND_URL}/api/auth/login`,
         { email, password }
       );
       if (response.data.success) {
